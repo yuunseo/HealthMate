89ebd6a039d8513893cd4160612feaf93b55732e
yuunseo <dbstj7457@naver.com>
2023-12-01 22:40:04 +0900

    헬스장 데이터 확인을 위한 사진 추가

--- health_mate/src/main/java/dao/GymRepository.java
+++ health_mate/src/main/java/dao/GymRepository.java
@@ -9,12 +9,15 @@ public class GymRepository {
 	public GymRepository() {
 		Gym ABC = new Gym("G0001","ABC");
 		ABC.setGymInfo("국내 최고 헬스장");
+		ABC.setFilename("G0001.jpg");
 		
 		Gym DEF = new Gym("G0002","DEF");
 		DEF.setGymInfo("최신 헬스 기기를 갖춘 헬스장");
+		DEF.setFilename("G0002.jpg");
 		
 		Gym GHI = new Gym("G0003","GHI");
 		GHI.setGymInfo("최고 트레이너 선생님들과 함께하는 헬스장");
+		GHI.setFilename("G0003.jpg");
 	
 		listOfGyms.add(ABC);
 		listOfGyms.add(DEF);
